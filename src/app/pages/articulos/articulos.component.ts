fa3c8f196fd69c06283bb4ac4c6a4462a6fde97b
antonio <antonio.ontiveros@ucb.edu.bo>
2024-11-25 18:10:29 -0400

    article element

--- src/app/pages/articulos/articulos.component.ts
+++ src/app/pages/articulos/articulos.component.ts
@@ -1,19 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { PostsService } from '../../servicios/posts.service';
+import { ArticuloComponent } from '../../elementos/articulo/articulo.component';
 
 @Component({
   selector: 'app-articulos',
   standalone: true,
-  imports: [],
+  imports: [ArticuloComponent],
   templateUrl: './articulos.component.html',
   styleUrl: './articulos.component.scss'
 })
 export class ArticulosComponent implements OnInit{
   constructor(private postsService: PostsService){ }
 
+  articulos: any
+
   ngOnInit(): void {
     this.postsService.getPosts().subscribe(
-      data => console.log(data),
+      data => this.articulos = data,
       error => console.log("HAY UN ERROR")
     )
   }
